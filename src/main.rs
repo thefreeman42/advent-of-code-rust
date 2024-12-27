470c1e22d96ec07c18d4ce8c5db63994b4e5df35
thefreeman42 <adamturczi@gmail.com>
2024-12-27 14:23:54 +0000

    Add user input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use core::DailyChallenge;
-
 use dotenv::dotenv;
+use std::io::stdin;
+use core::DailyChallenge;
 use one::HistorianHysteria;
 
 mod one;
@@ -9,8 +9,23 @@ mod core;
 fn main() {
   dotenv().ok();
 
-  let input = core::get_input(&1);
-  let challenge = HistorianHysteria::new(input);
+  println!("Which day would you like to run?");
+  let mut day_str = String::new();
+  stdin().read_line(&mut day_str).expect("Can't read input!");
+  
+  let day = day_str.trim().parse::<u16>().unwrap();
+  let challenge = get_challenge(day);
   println!("Part 1: {:?}", challenge.solve_part_one());
   println!("Part 2: {:?}", challenge.solve_part_two());
+}
+
+fn get_challenge(day: u16) -> impl DailyChallenge {
+  if day > 25 {
+    panic!("Day cannot be over 25!");
+  }
+  let input = core::get_input(&day);
+  match day {
+    1 => HistorianHysteria::new(input),
+    _ => panic!("Day {} not yet implemented!", day)
+  }
 }
